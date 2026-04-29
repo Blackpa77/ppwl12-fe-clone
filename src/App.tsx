e3738bbc8c2985808d61dd26361e0efb01c6f084
Blackpa77 <h1101241029@student.untan.ac.id>
2026-04-29 18:11:42 +0700

    perbaikan navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Page1 from './pages/Page1';
 import Page2 from './pages/Page2';
 import Navbar from './components/Navbar';
+import SpotifyNavbar from './components/SpotifyNavbar';
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <SpotifyNavbar />
       <Routes>
         <Route path="/" element={<Page1 />} />
         <Route path="/page-2" element={<Page2 />} />
