8257c05340e3abaf8355fa6dd7dc91a985f7dc22
Blackpa77 <h1101241029@student.untan.ac.id>
2026-04-29 14:58:54 +0700

    penambahan header 1.1

--- src/pages/Page2.tsx
+++ src/pages/Page2.tsx
@@ -1,7 +1,7 @@
 export default function Page2() {
-    return (
-        <div className="h-[calc(100vh-4rem)] flex justify-center items-center">
-            <h1>Page 2</h1>
-        </div>
-    )
+  return (
+    <div className="h-[calc(100vh-4rem)] flex justify-center items-center">
+      <h1>Page 2</h1>
+    </div>
+  )
 }
